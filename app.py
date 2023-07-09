fdf692e5dd9005820cdc8f2b704b1301d5ca946e
Monseleon21 <121038307@upq.edu.mx>
2023-07-08 23:59:20 -0600

    ultimo paso

--- app.py
+++ app.py
@@ -3,99 +3,100 @@ from flask_mysqldb import MySQL
 
 #Inicializacion del APP
 app= Flask (__name__)
-app.config['MYSQL_HOST']='localhost'
-app.config['MYSQL_USER']='root'
-app.config['MYSQL_PASSWORD']=''
-app.config['MYSQL_DB']='dbflask'
+app.config['MYSQL_HOST']="localhost"
+app.config['MYSQL_USER']="root"
+app.config['MYSQL_PASSWORD']=""
+app.config['MYSQL_DB']="DB_Fruteria"
 app.secret_key= 'mysecrety'
 mysql= MySQL(app)
 
-@app
+app.secret_key='mysecretkey'
+mysql= MySQL(app)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#Declaramos una ruta
 @app.route('/')
 def index():
   CC= mysql.connection.cursor();
-  CC.execute('select * from tbalbums')
-  conAlbums= CC.fetchall()
-  print(conAlbums)
-  return render_template('index.html',listAlbums= conAlbums)
+  CC.execute('select * from tbFrutas')
+  conFrutas= CC.fetchall()
+  print(conFrutas)
+  return render_template('index.html',listFrutas= conFrutas)
 
-#rura http:localhost:5000/guardar tipo POST para insert
-@app.route('/guardar',methods=['POST'])
-def guardar():
-  if request.method == 'POST':
-
-    # pasamos a variables el contenido de los input
-    Vtitulo=request.form['txtTitulo']
-    Vartista=request.form['txtArtista']
-    Vanio=request.form['txtAnio']
-   
-    #Conectar y ejecutar el insert
-    cs= mysql.connection.cursor()
-    cs.execute('insert into tbAlbums (titulo,artista,anio) values(%s,%s,%s)',(Vtitulo,Vartista,Vanio))
-    mysql.connection.commit()
-
-  flash('El album fue agregado correctamente')
-  return redirect(url_for('index'))
-
-@app.route('/editar/<string:id>')
-def editar(id):
-  cursoID=mysql.connection.cursor()
-  cursoID.execute('select * from tbAlbums where id= %s',(id,))
-  consultId=cursoID.fetchone()
-  return render_template('editarAlbum.html', album=consultId)
-
-@app.route('/actualizar/<id>',methods=['POST'])
-def actualizar(id):
-
-  if request.method == 'POST':
-    Vtitulo=request.form['txtTitulo']
-    Vartista=request.form['txtArtista']
-    Vanio=request.form['txtAnio']
-    curAct= mysql.connection.cursor()
-    curAct.execute('update tbAlbums set titulo= %s, artista= %s, anio= %s where id= %s',(Vtitulo,Vartista,Vanio,id))
-    mysql.connection.commit()
-
-    flash('Se ha actualizado el Album'+ Vtitulo)
+@app.route('/guardari', methods=['POST'])
+def guardari():
+    if request.method == 'POST':
+        Vfruta = request.form['txtFruta']
+        Vatemp = request.form['txtTemporada']
+        Vpr = request.form['txtPrecio']
+        Vst = request.form['txtStock']
+        
+        #print(Fruta, Temporada, Precio, Stock)
+        
+        #Conectamos y ejecutamos el Insert
+        CS = mysql.connection.cursor()
+        CS.execute('insert into tbFrutas (fruta,temporada,precio,stock) values (%s,%s,%s,%s)',(Vfruta,Vatemp,Vpr,Vst))
+        mysql.connection.commit()
+    flash('Fruta agregada correctamente ')
     return redirect(url_for('index'))
 
+@app.route('/editari/<string:id>')
+def editari(id):
+   cursoID=mysql.connection.cursor()
+   cursoID.execute('select*from tbFrutas where id=%s', (id,))
+   consultID=cursoID.fetchone()
 
-@app.route('/eliminar/<id>')
-def eliminar(id):
-    CID= mysql.connection.cursor()
-    CID.execute('Select * from tbAlbums where id = %s', (id,))
-    eliminarID= CID.fetchone()
-    return render_template('eliminarAlbum.html', album=eliminarID)
-  
+   return render_template('editar.html', consultID=consultID)
 
-@app.route('/borrar/<id>',methods=['POST'])
-def borrar(id):
-  
-  if request.method == 'POST':
-
-    curElim = mysql.connection.cursor()
-    curElim.execute('delete from tbAlbums where id = %s', (id,))
-    mysql.connection.commit()
-
-    flash('Se elimino el album')
+@app.route('/actualizari/<id>', methods=['POST'])
+def actualizari(id):
+    if request.method == 'POST':
+        Vfruta = request.form['txtFruta']
+        Vatemp = request.form['txtTemporada']
+        Vpr = request.form['txtPrecio']
+        Vst = request.form['txtStock']
+        
+        CS = mysql.connection.cursor()
+        CS.execute('update tbFrutas set fruta =%s ,temporada = %s ,precio = %s ,stock = %s where id = %s',(Vfruta,Vatemp,Vpr,Vst,id))
+        mysql.connection.commit()
+        
+    flash('Fruta actuializadfa correctamente en la Base de datos DB_Fruteria:')
     return redirect(url_for('index'))
-  
-#Ejecucion del servidor en el puerto 5000
-if __name__ == '__main__':
-  app.run(port=5000,debug=True)
+
+# Seleccionar el producto que se eliminara despúes
+@app.route('/eliminari/<id>')
+def eliminari(id):
+    CEID = mysql.connection.cursor()
+    CEID.execute('select * from tbFrutas where id = %s',(id,))
+    consultID = CEID.fetchone()
+    
+    return render_template('eliminar.html', consultID=consultID)
+
+# Eliminar de la lista 
+@app.route('/eliminarfruta/<id>', methods=['POST'])
+def eliminarafruta(id):
+    if request.method == 'POST':
+        
+        CE = mysql.connection.cursor()
+        CE.execute('delete from tbFrutas where id = %s',(id,))
+        mysql.connection.commit()
+        
+    flash('Se elimino la Fruta en la BD :')
+    return redirect(url_for('index'))
+    
+@app.route('/buscari', methods=['GET', 'POST'])
+def buscari():
+    if request.method == 'POST':
+        Vfruta = request.form['txtFruta']
+        
+        curslect = mysql.connection.cursor()
+        curslect.execute('SELECT * FROM tbFrutas WHERE fruta = %s', (Vfruta,))
+        CNT = curslect.fetchone()
+    
+    CS = mysql.connection.cursor()
+    CS.execute('SELECT fruta FROM tbFrutas')
+    frutas = CS.fetchall()
+    return render_template('buscar.html', list=frutas)
+
+#Ejecucion de servidor
+if __name__ =='__main__':
+    app.run(port=2000,debug=True)
